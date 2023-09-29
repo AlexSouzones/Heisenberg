ffabd2d7e201cec511acee0fe4740fcd54c569fa
Alexandre Pereira de Souza <alexandresouzones@gmail.com>
2023-09-28 23:03:50 -0300

    Correção Inicial

---
+++ core.py
@@ -0,0 +1,13 @@
+# Integração com redes sociais
+# - Discord
+# - Facebook
+# - Instagram
+# - Whatsapp
+# - Linkedin
+# - Github
+
+# Autonomia
+# Serviencia
+# Aprendizado
+# Copiloto de projetos
+# Integração a EQUIPAMENTOS FÍSICOS
